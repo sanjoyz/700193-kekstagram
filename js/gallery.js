0d28e6133676c46e6957c1415fceb5b1e49ee8a5
sanjoyz <sanjoyz1105@gmail.com>
2018-12-16 17:20:43 +0300

    aaaaaaaaaaaaaaaaaaaaa

--- js/gallery.js
+++ js/gallery.js
@@ -5,7 +5,8 @@
 */
 (function () {
   var PHOTOS_QUANTITY = 25;
-
+  var photos = [];
+  var pictureElements = document.querySelector('.pictures');
   var getPicture = function (photo) {
     var picturesTemplate = document.querySelector('#picture').content;
     var pictureEl = picturesTemplate.cloneNode(true);
@@ -22,15 +23,31 @@
     return pictureEl;
   };
 
-  var renderPictures = function (photos) {
+  var renderPictures = function (pic) {
     var picturesContainer = document.querySelector('.pictures');
     var pictureFragment = document.createDocumentFragment();
     for (var i = 0; i < PHOTOS_QUANTITY; i++) {
-      pictureFragment.appendChild(getPicture(photos[i]));
+      pictureFragment.appendChild(getPicture(pic[i]));
     }
     picturesContainer.appendChild(pictureFragment);
   };
 
+  var bigPicturePicker = function (array, src) {
+    for (var i = 0; i < PHOTOS_QUANTITY; i++) {
+      if (array[i].url === src) {
+        return array[i];
+      }
+    }
+    return 0;
+  };
+
+  var picturePreviewClickHandler = function (evt) {
+    var src = evt.target.attributes.src.nodeValue;
+    window.bigPicture.renderBigPicture(bigPicturePicker(photos, src));
+  };
+
+
   window.backend.download(renderPictures);
+  pictureElements.addEventListener('click', picturePreviewClickHandler);
 
 })();
