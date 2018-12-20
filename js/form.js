8442e46e3640393339329f23ac7cb1238b6db076
sanjoyz <sanjoyz1105@gmail.com>
2018-12-20 16:13:49 +0300

    валидация хештегов в forEach (не работает)

--- js/form.js
+++ js/form.js
@@ -52,6 +52,27 @@
     var outlineColorChanger = function (color) {
       target.style.outline = '1px solid' + color;
     };
+    arr.forEach(function (elem) {
+      if (elem[0] !== '#' && elem !== '') {
+        outlineColorChanger(HASHTAG_INVALID_COLOR);
+        return 'Хеш тег должен начинаться символом #';
+      } else if (elem.length > MAX_HASHTAG_LENGTH) {
+        outlineColorChanger(HASHTAG_INVALID_COLOR);
+        return 'Длина хеш тега не должна превышать ' + MAX_HASHTAG_LENGTH + ' ';
+      } else if (arr.length > MAX_HASHTAG_COUNT) {
+        outlineColorChanger(HASHTAG_INVALID_COLOR);
+        return 'Хеш тегов не может быть больше ' + MAX_HASHTAG_COUNT;
+      } else if (elem === '#' && elem.length < 2) {
+        outlineColorChanger(HASHTAG_INVALID_COLOR);
+        return 'Хеш тег не может состоять из одной решётки';
+      }
+      if (arr.indexOf(elem)) {
+        outlineColorChanger(HASHTAG_INVALID_COLOR);
+        return 'Один и тот же хеш-тег не может быть использован дважды';
+      }
+      return false;
+    });
+    /*
     for (var i = 0; i < arr.length; i++) {
       if (arr[i][0] !== '#' && arr[0] !== '') {
         outlineColorChanger(HASHTAG_INVALID_COLOR);
@@ -73,7 +94,9 @@
         }
       }
     }
+
     return false;
+    */
   };
 
   var hashTagsInputHandler = function (evt) {
